9c6b09f3db784b3fa4fd6a0fbde1c6ba786e4475
Emil Nymann <emilnymann96@gmail.com>
2019-09-11 13:48:36 +0200

    Removed public keywords from SmartLock class because it is only accessed in the same package.

--- src/SmartLock.java
+++ src/SmartLock.java
@@ -1,29 +1,29 @@
 import java.util.LinkedList;
 import java.util.List;
 
-public class SmartLock {
+class SmartLock {
 
     // Locked state (true = locked, false = unlocked)
     private boolean locked = true;
     // List of device identifiers that are allowed to unlock
     private List<String> whitelist = new LinkedList<>();
 
-    public SmartLock() {
+    SmartLock() {
     }
 
     // Check if the lock is locked or not.
-    public boolean isLocked() {
+    boolean isLocked() {
         return locked;
     }
 
     // Add a device identifier to the whitelist.
-    public void addToWhitelist(String device) {
+    void addToWhitelist(String device) {
         this.whitelist.add(device);
     }
 
     // Attempt to unlock with the passed in device identifier.
     // Unlock if the identifier is found in the whitelist, lock if it isn't.
-    public void attemptUnlock(String device) {
+    void attemptUnlock(String device) {
 
         boolean result = true;
 
