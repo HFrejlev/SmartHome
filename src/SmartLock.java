de512de7c50f99c7de3ff9f617c02c4e1228f80b
Emil Nymann <emilnymann96@gmail.com>
2019-09-11 12:41:32 +0200

    Add comments to to the SmartLock class.

--- src/SmartLock.java
+++ src/SmartLock.java
@@ -3,6 +3,7 @@ import java.util.List;
 
 public class SmartLock {
 
+    // Locked state (true = locked, false = unlocked)
     private boolean locked = true;
     // List of device identifiers that are allowed to unlock
     private List<String> whitelist = new ArrayList<>();
@@ -15,14 +16,18 @@ public class SmartLock {
         this.whitelist = whitelist;
     }
 
+    // Check if the lock is locked or not.
     public boolean isLocked() {
         return locked;
     }
 
+    // Add a device identifier to the whitelist.
     public void addToWhitelist(String device) {
         this.whitelist.add(device);
     }
 
+    // Attempt to unlock with the passed in device identifier.
+    // Unlock if the identifier is found in the whitelist, lock if it isn't.
     public void attemptUnlock(String device) {
 
         boolean result = true;
